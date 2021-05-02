41cf09a3075e893172ca6e9ee39be40d08206386
gvincenzi <giuseppe.vincenzi@vodafone.it>
2021-05-02 12:23:41 +0200

    What page in italian

---
+++ web/what/en.php
@@ -0,0 +1,29 @@
+<?php 
+$cookieDisclaimer = ("
+<h2>Che cos'è e perché CitizenRoom?</h2>
+Nel 1945 negli Stati Uniti nacque la <strong>Citizens' Band, la banda cittadina</strong>.<br>
+CB era uno dei vari servizi radio regolamentati dalla Federal Communications Commission (FCC) per consentire ai cittadini di utilizzare una banda di frequenze radio per la <strong>comunicazione personale</strong> (ad esempio i modellini radiocomandati, le chiacchiere fra parenti ed amici, le piccole imprese) <strong><a href='https://it.wikipedia.org/wiki/Banda_cittadina'> >> fonte Wikipedia</a></strong>.
+<br><br>
+CitizenRoom nasce con l'idea di riutilizzare lo stesso schema e proporre l'utilizzo di una parte del web in modo libero da qualsiasi rapporto con un intermediario pubblico o privato(a pagamento o gratuito ma in cambio di informazioni personali), sviluppando un servizio di comunicazione audio e video da dedicare alla comunicazione personale tra i cittadini.<br><br>
+CitizenRoom è una piattaforma per la creazione di stanze in cui poter poter organizzare delle riunioni (audio e video) con un numero teoricamente illimitato di partecipanti: il prototipo che è attualmente a vostra disposizione permette di invitare fino a <strong>30 persone</strong> per un corretto funzionamento.<br><br>
+CitizenRoom è <strong>gratuito, open source e non necessita né della creazione di un conto</strong>, né dell'installazione di applicaziioni (desktop o mobile): potete creare delle stanze liberamente e senza limiti e, utilizzando un tasto che vi genera un link di invito, potrete far partecipare chiunque senza obbligarlo a crearsi un conto o installare un software sul suo dispositivo.<br>
+Abbiamo sentito l'esigenza di creare e testare una piattaforma che puntasse piuttosto sulla <strong>semplicità</strong> che sulla personalizzazione del servizio.<br><br>
+<h2>Gestione dei dati</h2>
+CitizenRoom non salva <strong>nessun dato degli utenti, dei dispositivi, della conversazione</strong>. Questo non significa che CitizenRoom sia un'applicazione in cui poter godere di un totale anonimato, al contrario: CitizenRoom è una piattaforma <strong>libera ma non cifrata</strong>, quindi in ogni momento, in caso di scambio di informazioni scorretto e/o illegale, <strong>ogni conversazione potrà essere monitorata da un'autorità competente</strong>.<br>
+CitizenRoom vuole essere un mezzo semplice e immediato, ma non uno strumento di offuscamento della propria identità per fini illeciti e contrari alla legge dello Stato in cui la piattaforma è situata e da cui gli utenti si connettono.<br><br>
+<h2>CitizenRoom Business</h2>
+Stiamo testando anche una versione (<strong>sempre gratuita</strong>) che abbiamo definito <strong>Business</strong> nella quale potrete crearvi un conto (inserendo solo una mail valida): con la creazione di questo conto potrete creare delle stanze proprie al vostro gruppo di lavoro, alla vostra associazione, alla vostra famiglia.<br>
+Esempio: se create una stanza numero 35, questa 35 sarà solo vostra e non condividerà i partecipanti con la stanza 35 della versione di CitizenRoom normale.<br><br>
+Nella versione Business avrete la possibilità di personalizzare le stanze con un titolo ed un logo e di utilizzare 3 livelli di accesso alle vostre stanze:<br>
+<ul>
+<li>Con richiesta di ingresso</li>
+<li>Con password di accesso</li> 
+<li>Con ticket (lista predefinita di nickname invitati)</li>
+</ul>
+<h2>Un prototipo Open Source</h2>
+CitizenRoom è un prototipo di piattaforma per conferenze web, basato sull'API pubblica di <a href='https://meet.jit.si/'>Jitsi Meet</a>, scritto in PHP/Javascript e completamente Open Source.<br>
+Il codice del prototipo potete leggerlo e eventualmente proporre di modificarlo su GitHub nel <a href='https://github.com/gvincenzi/citizenroom'>repository dedicato</a>.
+<br><br>
+
+");
+?>
---
+++ web/what/index.php
@@ -0,0 +1,44 @@
+<?php
+include_once '../../server/admin/lang.php';
+include '../../server/admin/langs/' . prefered_language ( $available_languages ) . '.php';
+include prefered_language ( $available_languages ) . '.php';
+
+session_start();
+?>
+<html lang="en">
+<head>
+	<meta charset="utf8">
+	<title><?php print $lang['PAGE_TITLE']?></title>
+	<meta property="og:title" content="<?php print $lang['PAGE_TITLE']?>" />
+		
+	<!-- Facebook Meta Tags -->
+	<meta property="og:url" content="https://citizenroom.altervista.org/web/what/">
+	<meta property="og:type" content="website">
+	<meta name="description" property="og:description" content="Cos'è e perché CitizenRoom - What and why CitizenRoom (italian language) - Qu'est-ce que et pourquoi CitizenRoom (en italien)">
+	<meta property="og:image" content="https://citizenroom.altervista.org/web/assets/img/icon.png">
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image">
+	<meta property="twitter:domain" content="citizenroom.altervista.org">
+	<meta property="twitter:url" content="https://citizenroom.altervista.org/web/what/">
+	<meta name="twitter:title" content="CitizenRoom - A free space where you can feel at home">
+	<meta name="twitter:description" content="Informations about CitizenRoom (italian language)">
+	<meta name="twitter:image" content="https://citizenroom.altervista.org/web/assets/img/icon.png">
+
+
+	<script src="https://code.jquery.com/jquery-3.2.1.js"
+		integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
+		crossorigin="anonymous"></script>
+    
+    <link href="../assets/css/what.css" rel="stylesheet">
+
+</head>
+
+<body style="background-color: #f5f5f5">   
+  <?php include '../header.php';?> 
+  
+  <div class="what">
+  <?php print $cookieDisclaimer;?>
+  </div>
+</body>
+</html>

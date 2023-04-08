0e3c640b9bf14237e8082faec0c16b9f6f8def96
Giuseppe Vincenzi <gdvincenzi@airfrance.fr>
2023-04-09 00:24:37 +0200

    Normalization menu files

--- web/menu_public.php
+++ web/menu_public.php
@@ -6,6 +6,7 @@
 		<ul>
 			<nav classname="site-nav">
 				<ul>
+                    <li class=""><a href="../join"><?php print $lang['JOIN']?></a></li>
 					<li class=""><a href="../civichall?room_type=civic_hall"><?php print $lang['CIVIC_HALL']?></a></li>
 					<li class=""><a href="../custom?room_type=custom"><?php print $lang['CUSTOM_ROOM']?></a></li>
 					<li class=""><a href="../themed?room_type=themed"><?php print $lang['THEMED_ROOM']?></a></li>

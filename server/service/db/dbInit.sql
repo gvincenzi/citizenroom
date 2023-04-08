c661233ba508649f72099a1300a0414bae16260b
Giuseppe Vincenzi <gdvincenzi@airfrance.fr>
2023-04-09 00:29:59 +0200

    SQL script init

--- server/service/db/dbInit.sql
+++ server/service/db/dbInit.sql
@@ -19,6 +19,18 @@ CREATE TABLE IF NOT EXISTS `citizenroom_subscription` (
   `creation_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+CREATE TABLE IF NOT EXISTS `citizenroom_theme` (
+  `room_id` int(10) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `info` varchar(50) NOT NULL,
+  `image` varchar(2048) NOT NULL,
+  `bg_image` varchar(2048) NOT NULL,
+  `bg_image_link` varchar(2048) NOT NULL,
+  `bg_image_author` varchar(255) NOT NULL,
+  `bg_image_author_link` varchar(2048) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

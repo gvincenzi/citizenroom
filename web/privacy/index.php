929b382e6a93256026483c83a61853a088b84947
Giuseppe Vincenzi <gdvincenzi@airfrance.fr>
2023-12-13 22:45:41 +0100

    Privacy and About us pages

--- web/privacy/index.php
+++ web/privacy/index.php
@@ -18,8 +18,28 @@ session_start();
 <body style="background-color: #f5f5f5">   
   <?php include '../header.php';?> 
   
-  <div class="what">
-  <?php print $cookieDisclaimer;?>
+  <div class="col-md-5 ml-auto mr-auto">
+			<div class="card card-plain">
+        <div class="card-header text-center">
+						<div class="logo" id="title">CitizenRoom</div>
+						<div id="primary-navigation-menu">
+							<nav>
+								<ul class="nav justify-content-center">
+									<nav classname="nav-item">
+										<ul>
+											<li class="nav-link"><a href="../what" class="link menu-link"><?php print $lang['ABOUT']?></a></li>
+											<li class="nav-link"><a href="../privacy" class="link menu-link">Privacy (italian language)</a></li>
+										</ul>
+									</nav>
+								</ul>
+							</nav>
+            </div>
+        </div>
+    </div>
   </div>
+  <div class="what">
+    <?php print $cookieDisclaimer;?>
+  </div>
+        
 </body>
 </html>

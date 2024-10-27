48e440b34902568964ba8fc5c516cbd5df75439e
Giuseppe Vincenzi <gdvincenzi@airfrance.fr>
2024-10-27 12:24:49 +0100

    Open data + Room Type french_national_assembly_delegate

--- server/service/api/API.php
+++ server/service/api/API.php
@@ -12,10 +12,13 @@ if (isset($_REQUEST['method'] )){
 		case 'join':
 			if(isset($_REQUEST['room_type']) && $_REQUEST['room_type']=='custom'){
               	$api->joinCustom($_REQUEST['room_title'], $_REQUEST['room_logo'], $_REQUEST['nickname'], $_REQUEST['room_id'], $_REQUEST['room_type'], $link);
-            } else if(isset($_REQUEST['room_type']) && $_REQUEST['room_type']=='public'){
+            } else if(isset($_REQUEST['room_type']) && $_REQUEST['room_type']=='french_national_assembly'){
+				$api->joinFrenchNationalAssembly($_REQUEST['nickname'], $_REQUEST['room_id'], $_REQUEST['room_type'], $link);
+			} else if(isset($_REQUEST['room_type']) && $_REQUEST['room_type']=='public'){
 				$api->join($_REQUEST['nickname'], $_REQUEST['room_id'], $_REQUEST['room_type'], $link);
 			}
 			break;
+		case 'joinFrenchNationalAssemblyInit': $api->joinFrenchNationalAssemblyInit(); break;
 		case 'left':
 			$api->leftRoom($link);
 			break;
@@ -50,6 +53,47 @@ class API {
 		$_SESSION['room_logo'] = $room_logo;
 
 		$this->join($nickname,$room_id,$room_type,$link);
+	}
+
+	public function joinFrenchNationalAssembly($nickname,$room_id,$room_type,$link){
+		unset($_SESSION['room_title']);
+		unset($_SESSION['room_logo']);
+
+		//READ DATA
+		$file = fopen("../../data/liste_deputes_libre_office.csv","r");
+		$keys = fgetcsv($file);
+		$french_national_assembly_delegates = [];
+		while ($line = fgetcsv($file)) {
+			$data = array_combine($keys, $line);
+			$french_national_assembly_delegates[] = $data;
+		}
+		fclose($file);
+
+		foreach($french_national_assembly_delegates as $french_national_assembly_delegate){
+			if($french_national_assembly_delegate['uid'] == $room_id)
+			{
+				$_SESSION['room_title'] = stripslashes($french_national_assembly_delegate['firstname'].' '.$french_national_assembly_delegate['lastname']);
+				$_SESSION['room_logo'] = "https://www2.assemblee-nationale.fr/static/tribun/17/photos/".$french_national_assembly_delegate['uid'].".jpg";
+				break;
+			}
+		}
+
+		$this->join($nickname,$room_id,$room_type,$link);
+	}
+
+	public function joinFrenchNationalAssemblyInit(){
+		
+		//READ DATA
+		$file = fopen("../../data/liste_deputes_libre_office.csv","r");
+		$keys = fgetcsv($file);
+		$french_national_assembly_delegates = [];
+		while ($line = fgetcsv($file)) {
+			$data = array_combine($keys, $line);
+			$french_national_assembly_delegates[] = $data;
+		}
+		fclose($file);
+		$tojson = json_encode($french_national_assembly_delegates,JSON_UNESCAPED_UNICODE);
+		print $tojson;
 	}
 
 	public function leftRoom($link){
--- web/bootstrap.php
+++ web/bootstrap.php
@@ -3,6 +3,7 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
 
 <script src="../assets/js/general.js?v=<?php print time()?>"></script>
 
@@ -11,3 +12,4 @@
 <link href="../assets/css/header.css?v=<?php print time()?>" rel="stylesheet">
 <link href="../assets/css/menu.css?v=<?php print time()?>" rel="stylesheet">
 <link href="../assets/css/subtitle.css?v=<?php print time()?>" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
